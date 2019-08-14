baf2903eaf28c0b160bce9974d02090c9052dcb0
kaioshin <krzysztof.kaszuba@o2.pl>
2019-08-14 20:40:52 +0200

    wip

--- test_deep_xor.py
+++ test_deep_xor.py
@@ -4,15 +4,9 @@ import layers
 import functions
 import loss_functions
 
-<<<<<<< Updated upstream
 loss = loss_functions.MSE()
 
-net = layers.Network(layers.SGDMomentum(0.5,0.9))
-=======
-
-
-net = layers.Network(layers.Adam(0.01))
->>>>>>> Stashed changes
+net = layers.Network(layers.Adam(0.05))
 net.add(layers.Dense(2, 5, functions.LeakyReLu(), '1st hidden'))
 net.add(layers.Dense(5, 20, functions.LeakyReLu(), '1st hidden'))
 net.add(layers.Dense(20, 5, functions.LeakyReLu(), '2nd hidden'))
@@ -23,7 +17,7 @@ inp = np.asarray([[0 , 0], [1, 0], [0, 1], [1, 1]])
 target = np.asarray([0, 1, 1, 0])
 
 print("START")
-for _ in range(1000):
+for i in range(1000):
     """for i, inputX in enumerate(inp):
         result = net.propagate(inputX)
         #print(str(target[i]) + " : " + str(result))
@@ -31,7 +25,9 @@ for _ in range(1000):
         #print(np.sum(dL_dO ** 2))
         net.backpropagate(dL_dO)"""
     err, _ = net.batch_fit(inp, target, loss)
-    print("Error : {}".format(err))
+    if err < 0.0001:
+        print("Finished Err = {} iterations : {}".format(err, i))
+        break
 
 for i, inputX in enumerate(inp):
         result = net.propagate(inputX)
